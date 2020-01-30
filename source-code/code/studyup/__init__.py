88c098dc6b6ef52ca62f4dba78eb660d886b2ffc
katrina <kat.kopio@gmail.com>
2020-01-31 01:03:06 +0800

    Adding question input
    Doesn't work yet

--- source-code/code/studyup/__init__.py
+++ source-code/code/studyup/__init__.py
@@ -1,13 +1,24 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
 from studyup.config import Config
+from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
 
 db = SQLAlchemy()
 
+from studyup.question.routes import photos
+
 def create_app(config_class=Config):
+    basedir = os.path.abspath(os.path.dirname(__file__))
+
     app = Flask(__name__)
+
+    app.config['UPLOADED_PHOTOS_DEST'] = os.path.join(basedir, 'static/img')
     app.config.from_object(Config)
+
+    configure_uploads(app, photos)
+    patch_request_class(app)  # set maximum file size as 16MB
 
     db.init_app(app) 
 
