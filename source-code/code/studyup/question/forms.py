b4abbacd67f41c2914a06aa50f3d75929e020221
karinakylle <54421676+karinakylle@users.noreply.github.com>
2020-01-31 05:02:04 +0800

    Update forms.py

--- source-code/code/studyup/question/forms.py
+++ source-code/code/studyup/question/forms.py
@@ -1,3 +1,22 @@
+"""
+LICENSE
+This is a course requirement for CS 192 Software Engineering II 
+under the supervision of Asst. Prof. Ma. Rowena C. Solamo 
+of the Department of Computer Science, College of Engineering, 
+University of the Philippines, Diliman for the AY 2019-2020
+AUTHORS: Ang, Karina Kylle L. 
+         Kopio, Katrina Mae D. 
+         Principio, Roberto Jr. D.
+CODE HISTORY
+01-23-2020 Principio - File created, code added, documentation added
+INFO
+File Creation Date: January 24, 2020
+Development Group: Group 9 - StudyUP Team
+Client Group: Ma'am Solamo, CS 192 Class
+Purpose of the Software: To provide a collaborative learning
+    environment in the courses of UP Diliman.
+"""
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, RadioField
 from flask_wtf.file import FileField, FileAllowed
